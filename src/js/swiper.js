9f657fbdadea0295c8dbc613a16c761bb0a05d0f
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-07-18 17:44:49 +0900

    🚧 wip: Swiper初期設定+α #12

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -3,4 +3,23 @@ import { register } from "swiper/element/bundle";
 // register Swiper custom elements
 register();
 
-console.log("すわすわ");
+const swiperEl = document.querySelector("swiper-container");
+
+// swiper parameters
+const swiperParams = {
+  slidesPerView: 1,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+};
+
+// now we need to assign all parameters to Swiper element
+Object.assign(swiperEl, swiperParams);
+
+// and now initialize it
+swiperEl.initialize();
