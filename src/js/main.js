be2744179686bac2370247d948c577a020e4adfb
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-07-18 17:33:07 +0900

    🚧 wip: JavaScript追加 #18

--- src/js/main.js
+++ src/js/main.js
@@ -2,31 +2,35 @@ import "./../scss/style.scss";
 import * as bootstrap from "bootstrap";
 import "../../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 
-// ヒーローエリアの高さを過ぎたら、navの背景色を変える
-// ヒーローエリアの高さを取得
-// スクロールの高さを取得、一定の範囲を超えたらイベント発火、クラスをつける
-// 一定の範囲を戻ったら、クラスを外す
-// ナビを開いた時に背景色を変える
-
-const menuDom = document.querySelectorAll("#menu li a");
-console.log(menuDom);
-
-// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetHeight
-
-const promotionDom = document.getElementById("promotion");
-console.log(promotionDom.offsetTop);
-const foodCategoryDom = document.getElementById("food-category");
-console.log(foodCategoryDom.offsetTop);
-
 document.addEventListener("scroll", (event) => {
   const position = window.scrollY;
-  console.log(position);
-
-  if (position > promotionDom.offsetTop && position < promotionDom.offsetTop + promotionDom.offsetHeight) {
-    menuDom[1].classList.add("active");
-    console.log("he");
-  } else {
-    menuDom[1].classList.remove("active");
-    console.log("のおお");
-  }
+  acitivateMenu(position, "hero", 0);
+  acitivateMenu(position, "promotion", 1);
+  acitivateMenu(position, "food-category", 2);
+  acitivateMenu(position, "faq", 3);
 });
+
+function acitivateMenu(currentPosition, elementId, menuOrder) {
+  const menu = document.querySelectorAll("#menu li a");
+  const element = document.getElementById(elementId);
+
+  const elementStartXPosition = element.offsetTop - 1;
+  const elementEndXPosition = element.offsetTop + element.offsetHeight - 1;
+  if (currentPosition > elementStartXPosition && currentPosition < elementEndXPosition) {
+    menu[menuOrder].classList.add("active");
+    setAriaCurrent(menuOrder);
+  } else {
+    menu[menuOrder].classList.remove("active");
+    removeAriaCurrent(menuOrder);
+  }
+}
+
+function setAriaCurrent(menuOrder) {
+  const menu = document.querySelectorAll("#menu li a");
+  menu[menuOrder].ariaCurrent = true;
+}
+
+function removeAriaCurrent(menuOrder) {
+  const menu = document.querySelectorAll("#menu li a");
+  menu[menuOrder].ariaCurrent = false;
+}
