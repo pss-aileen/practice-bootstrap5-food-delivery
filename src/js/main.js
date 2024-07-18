ac86eec7df0c1c0c807ddabdc4952ae1d4109a22
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-07-18 17:34:55 +0900

    🎨 style: Navのアクセシビリティ向上 #18 (#22)
    * 🚧 wip: ナビのアンカーリンク追加 #18
    
    * 🚧 wip: スクロールした位置に応じてアンカーリンクにクラス追加仮実装 #18
    
    promotionだけできたので、他もやる
    
    * 🚧 wip: JavaScript追加 #18

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,36 @@
 import "./../scss/style.scss";
 import * as bootstrap from "bootstrap";
 import "../../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+
+document.addEventListener("scroll", (event) => {
+  const position = window.scrollY;
+  acitivateMenu(position, "hero", 0);
+  acitivateMenu(position, "promotion", 1);
+  acitivateMenu(position, "food-category", 2);
+  acitivateMenu(position, "faq", 3);
+});
+
+function acitivateMenu(currentPosition, elementId, menuOrder) {
+  const menu = document.querySelectorAll("#menu li a");
+  const element = document.getElementById(elementId);
+
+  const elementStartXPosition = element.offsetTop - 1;
+  const elementEndXPosition = element.offsetTop + element.offsetHeight - 1;
+  if (currentPosition > elementStartXPosition && currentPosition < elementEndXPosition) {
+    menu[menuOrder].classList.add("active");
+    setAriaCurrent(menuOrder);
+  } else {
+    menu[menuOrder].classList.remove("active");
+    removeAriaCurrent(menuOrder);
+  }
+}
+
+function setAriaCurrent(menuOrder) {
+  const menu = document.querySelectorAll("#menu li a");
+  menu[menuOrder].ariaCurrent = true;
+}
+
+function removeAriaCurrent(menuOrder) {
+  const menu = document.querySelectorAll("#menu li a");
+  menu[menuOrder].ariaCurrent = false;
+}
