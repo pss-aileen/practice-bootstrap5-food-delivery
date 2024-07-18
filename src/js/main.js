9f3c35ea3277c5cdbeb38e7f645ed738471cce48
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-07-18 11:32:04 +0900

    🚧 wip: スクロールした位置に応じてアンカーリンクにクラス追加仮実装 #18
    promotionだけできたので、他もやる

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,32 @@
 import "./../scss/style.scss";
 import * as bootstrap from "bootstrap";
 import "../../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+
+// ヒーローエリアの高さを過ぎたら、navの背景色を変える
+// ヒーローエリアの高さを取得
+// スクロールの高さを取得、一定の範囲を超えたらイベント発火、クラスをつける
+// 一定の範囲を戻ったら、クラスを外す
+// ナビを開いた時に背景色を変える
+
+const menuDom = document.querySelectorAll("#menu li a");
+console.log(menuDom);
+
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetHeight
+
+const promotionDom = document.getElementById("promotion");
+console.log(promotionDom.offsetTop);
+const foodCategoryDom = document.getElementById("food-category");
+console.log(foodCategoryDom.offsetTop);
+
+document.addEventListener("scroll", (event) => {
+  const position = window.scrollY;
+  console.log(position);
+
+  if (position > promotionDom.offsetTop && position < promotionDom.offsetTop + promotionDom.offsetHeight) {
+    menuDom[1].classList.add("active");
+    console.log("he");
+  } else {
+    menuDom[1].classList.remove("active");
+    console.log("のおお");
+  }
+});
